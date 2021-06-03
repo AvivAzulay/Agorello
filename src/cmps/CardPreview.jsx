bf41fe83aff9935718820d93e311e33ff3f3e288
gad <studio360r@gmail.com>
2021-06-03 17:34:40 +0300

    43

--- src/cmps/CardPreview.jsx
+++ src/cmps/CardPreview.jsx
@@ -2,16 +2,10 @@ import React from 'react'
 // import Checkbox from './Checkbox';
 import { Link } from 'react-router-dom'
 import { Draggable } from 'react-beautiful-dnd'
-import { MemberIcon } from './MemberIcon'
+import {MemberIcon} from './MemberIcon'
 
-export function CardPreview({ onRemoveCard, card, index, onSaveCard, getActivitiesByCardId, onOpenPreviewLabels, isLebelOpen, board, toggleDueDate }) {
-
-
-    function toggleDueDate(ev) {
-        ev.stopPropagation()
-        card.dueDate.isCompleted = !card.dueDate.isCompleted
-        onSaveCard(card, card.currGroup.groupId)
-    }
+export function CardPreview({ onRemoveCard, card, index, getActivitiesByCardId, onOpenPreviewLabels, isLebelOpen, board
+}) {
 
     return (
 
@@ -42,27 +36,31 @@ export function CardPreview({ onRemoveCard, card, index, onSaveCard, getActiviti
                             <div className="test-white-space">{card.title} </div>
 
                             <div className="card-preview-attachments" >{
-
                                 card.attachments.map((attachment, index) =>
                                     <img className="card-preview-attachments-img" src={attachment} alt="" key={index} />)}
                             </div>
 
                             <div className="card-preview-bottom">
-                                {getActivitiesByCardId(card.id).length !== 0 &&
+                               <div className="card-preview-bottom-list">
+                               {getActivitiesByCardId(card.id).length !== 0 &&
                                     <span className="card-preview-activities ">{getActivitiesByCardId(card.id).length}</span>}
-                                {card?.dueDate?.time ? <span onClick={(event) => toggleDueDate(event)} className={card.dueDate.isCompleted ? "card-preview-date checked" : "card-preview-date not-checked"}>
+                                {card.dueDate ? <span className="card-preview-date checked">
+
                                     <div className="card-preview-date-clock"></div>
-                                    {(new Date(card.dueDate.time)).toString().split(' ')[1]
+                                    {(new Date(card.dueDate)).toString().split(' ')[1]
                                         + ' ' +
-                                        (new Date(card.dueDate.time)).getUTCDate()
+                                        (new Date(card.dueDate)).getUTCDate()
                                     }</span> : <span></span>}
                                 {card.description && <span className="icon-desription"></span>}
-
+                               </div>
+                               
                                 <div className="card-preview-members">{
                                     card.members &&
                                     card.members.map((member, index) =>
                                         // <div key={index}>{member.fullname.split(' ').map(name => name[0]).slice(0, 2)[0] + member.fullname.split(' ').map(name => name[0]).slice(0, 2)[1]}</div>
-                                        <MemberIcon member={member} />
+                                     
+                                        <MemberIcon member={member}/>
+
                                     )}
                                 </div>
 
